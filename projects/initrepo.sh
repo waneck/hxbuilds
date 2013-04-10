327f3f8fcea02440f061305c9768096b71e4130d
Caue Waneck <waneck@hxdev.(none)>
2013-04-10 12:15:59 -0400

    first checkout will need a build also

--- projects/initrepo.sh
+++ projects/initrepo.sh
@@ -5,3 +5,5 @@ git svn clone https://haxe.googlecode.com/svn/trunk haxe/repo/haxe
 git svn clone https://ocamllibs.googlecode.com/svn/trunk/ haxe/repo/ocamllibs
 git svn clone https://nekovm.googlecode.com/svn/trunk neko/repo/neko
 ln -s ../ocamllibs haxe/repo/haxe/libs
+touch haxe/repo/.updated
+touch neko/repo/.updated
