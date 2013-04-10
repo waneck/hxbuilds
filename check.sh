0e80b5448d6ea7f468cdd808878121eef0661801
Caue Waneck <waneck@hxdev.(none)>
2013-04-10 10:07:53 -0400

    working haxe/neko

--- check.sh
+++ check.sh
@@ -24,14 +24,14 @@ for proj in projects/*; do
 					./build.sh $REV && touch build/.r$REV || PROJEXITVAR=1
 				fi
 				if [ -f build/.r$REV ]; then
-					tar -zcvf out/${PROJECT}_r$REV.tar.gz build/*
+					tar -zcvf out/${PROJECT}_r$REV.tar.gz -C build .
 				else
 					PROJEXITVAR=1
 				fi
 
 				if [ -f out/${PROJECT}_r$REV.tar.gz ]; then
 					cd ../../../..
-					./sync.sh $BASE/$proj/${plat}/out/${PROJECT}_r$REV.tar.gz $(basename ${plat})/${PROJECT}_r$REV.tar.gz || EXITVAR=1
+					./sync.sh $BASE/$proj/${plat}/out/${PROJECT}_r$REV.tar.gz $(basename $proj)/$(basename ${plat})/${PROJECT}_r$REV.tar.gz || EXITVAR=1
 				else
 					PROJEXITVAR=1
 				fi
