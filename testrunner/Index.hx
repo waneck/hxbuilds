f1a7dbba6302da5ee50ac2642cf252056e99a1a7
Caue Waneck <waneck@gmail.com>
2013-04-17 11:52:34 -0300

    frontend: result view, and view specific revision

--- testrunner/Index.hx
+++ testrunner/Index.hx
@@ -5,6 +5,7 @@ import sys.db.Manager;
 import db.TestResult;
 
 using Lambda;
+using StringTools;
 
 class Index
 {
@@ -16,7 +17,10 @@ class Index
 	static function main()
 	{
 		Env.init();
-		Dispatch.run(Web.getURI(), Web.getParams(), new Index());
+		var uri = Web.getURI();
+		if (uri.endsWith("index.n"))
+			uri = uri.substr(0,uri.length-7);
+		Dispatch.run(uri, Web.getParams(), new Index());
 		Manager.cnx.close();
 	}
 
@@ -28,14 +32,38 @@ class Index
 		for (rev in revs)
 		{
 			projs.push({
-				title: rev.project + " (" + rev.revision + ")",
+				title: rev.project + " " + rev.revision,
 				tests: getTestsObject(rev)
 			});
 		}
 		Sys.println(new view.TestView().setData({ projects: projs }).execute());
 	}
 
-	static function getTestsObject(rev:Revision):Array<{ title:String, targets:Array<{ name:String, results:Array<String> }> }>
+	public function doP(project:String, revision:String)
+	{
+		var rev = Revision.manager.select($project == project && $revision == revision,null,false);
+		var projs =[{ title: rev.project + " " + rev.revision, tests:getTestsObject(rev) }];
+		Sys.println(new view.TestView().setData({ projects: projs }).execute());
+	}
+
+	public function doResult(r:TestResult)
+	{
+		var view = {
+			testPart: r.testPart.title,
+			target: r.target,
+			project: r.revision.project,
+			revision: r.revision.revision,
+
+			stdout: r.stdout,
+			stderr: r.stderr,
+
+			results: TestResult.manager.search($testPart == r.testPart && $target == r.target && $revision_id < r.revision.id, { limit:20 }, false)
+				.map(function(r) return { rev: r.revision.revision, date: r.dateRan, status: (r.success ? "gtest" : "rtest"), id: r.id }).array()
+		};
+		Sys.println(new view.ResultView().setData(view).execute());
+	}
+
+	static function getTestsObject(rev:Revision):Array<{ title:String, targets:Array<{ name:String, results:Array<{cls:String, id:Int}> }> }>
 	{
 		var parts = null, ret = [], curTarget = null, curTest = null, cur = null;
 		for (r in Manager.cnx.request('SELECT t1.*, t2.title as part_title, t2.name as part_name, t3.title as test_title, t3.name as test_name FROM TestResult t1 JOIN TestPart t2 ON t1.testPart_id = t2.id JOIN Test t3 ON t2.test_id = t3.id WHERE revision_id = ' + Manager.quoteAny(rev.id) + ' ORDER BY t3.name, t1.target'))
@@ -59,8 +87,20 @@ class Index
 			if (targetPos < 0)
 				targetPos = parts.push(r.part_name) - 1;
 			var suc = untyped r.success || r.success == 1; //sqlite vs mysql
-			cur.targets[cur.targets.length - 1].results[targetPos] = (suc ? "gtest" : "rtest");
+			cur.targets[cur.targets.length - 1].results[targetPos] = { cls:(suc ? "gtest" : "rtest"), id:r.id };
 		}
+
+		for(r in ret)
+			for(t in r.targets)
+			{
+				for(i in 0...t.results.length)
+				{
+					if (t.results[i] == null)
+					{
+						t.results[i] = { cls: "ytest", id:0 };
+					}
+				}
+			}
 		return ret;
 	}
 
