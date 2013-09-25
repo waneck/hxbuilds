9a793ac2d323cf2b4802ebb85c702c84d46d3563
Caue Waneck <waneck@gmail.com>
2013-09-25 15:38:17 -0300

    no need for writing permissions there

--- projects/initrepo.sh
+++ projects/initrepo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p haxe/repo
 mkdir -p neko/repo
-git clone --recursive git@github.com:HaxeFoundation/haxe.git haxe/repo/haxe
-git clone --recursive git@github.com:HaxeFoundation/neko.git neko/repo/neko
+git clone --recursive https://github.com/HaxeFoundation/haxe.git haxe/repo/haxe
+git clone --recursive https://github.com/HaxeFoundation/neko.git neko/repo/neko
 touch haxe/repo/.updated
 touch neko/repo/.updated
