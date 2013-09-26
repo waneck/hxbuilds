b084661e23abda3871eb044fb329372e0eb18d31
Caue Waneck <waneck@gmail.com>
2013-09-25 23:11:10 -0300

    windows support

--- projects/haxe/check.sh
+++ projects/haxe/check.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd repo/haxe
 OLDREV=$(git rev-parse HEAD)
+git submodule update
 git pull
+git submodule update
 REV=$(git rev-parse HEAD)
 if [ ! $OLDREV = $REV ]; then
 	touch ../.updated
--- projects/haxe/platforms/windows-mingw-linux/build.sh
+++ projects/haxe/platforms/windows-mingw-linux/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-LNX=$PWD
+WIN=$PWD
 
 if [ $# -eq 0 ]; then
-	echo "No arguments supplied: $1"
-	exit 1
+        echo "No arguments supplied: $1"
+        exit 1
 fi
 
 cd ../../repo/haxe
-rm haxe
-make clean && make all && cp haxe $LNX/build/haxe && cp -rf std $LNX/build/ && exit 0
+rm -f haxe*
+make clean && make "OCAMLOPT=i686-w64-mingw32-ocamlopt" "OCAMLC=i686-w64-mingw32-ocamlc" && cp haxe $WIN/build/haxe.exe && cp -rf std $WIN/build/ && echo "haxe --run tools.haxelib.Main %*" > $WIN/build/haxelib.bat && exit 0
 exit 1
