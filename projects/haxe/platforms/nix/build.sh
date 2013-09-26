61484cf1b89ebd4695d46ca8ba6075a2aa49161e
Caue Waneck <waneck@gmail.com>
2013-09-26 07:56:54 -0300

    Build revision number; changed naming scheme

--- projects/haxe/platforms/nix/build.sh
+++ projects/haxe/platforms/nix/build.sh
@@ -9,5 +9,5 @@ fi
 
 cd ../../repo/haxe
 rm haxe*
-make clean && make all && make tools && cp haxe* $LNX/build/ && cp -rf std $LNX/build/ && exit 0
+make clean && make all "ADD_REVISION=1" && make tools && cp haxe* $LNX/build/ && cp -rf std $LNX/build/ && exit 0
 exit 1
--- projects/haxe/platforms/windows-mingw-linux/build.sh
+++ projects/haxe/platforms/windows-mingw-linux/build.sh
@@ -9,5 +9,5 @@ fi
 
 cd ../../repo/haxe
 rm -f haxe*
-make clean && make "OCAMLOPT=i686-w64-mingw32-ocamlopt" "OCAMLC=i686-w64-mingw32-ocamlc" && cp haxe $WIN/build/haxe.exe && cp -rf std $WIN/build/ && echo "haxe --run tools.haxelib.Main %*" > $WIN/build/haxelib.bat && exit 0
+make clean && make "ADD_REVISION=1" "OCAMLOPT=i686-w64-mingw32-ocamlopt" "OCAMLC=i686-w64-mingw32-ocamlc" && cp haxe $WIN/build/haxe.exe && cp -rf std $WIN/build/ && echo "haxe --run tools.haxelib.Main %*" > $WIN/build/haxelib.bat && exit 0
 exit 1
