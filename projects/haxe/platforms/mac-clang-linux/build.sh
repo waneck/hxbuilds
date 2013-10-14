17f3507450fe8cc91ef18b82a7ba9211d9d49224
Caue Waneck <waneck@gmail.com>
2013-10-14 00:16:15 -0300

    deployment target set to 10.5

--- projects/haxe/platforms/mac-clang-linux/build.sh
+++ projects/haxe/platforms/mac-clang-linux/build.sh
@@ -9,5 +9,6 @@ fi
 
 cd ../../repo/haxe
 rm -f haxe*
+export MACOSX_DEPLOYMENT_TARGET=10.5
 make clean && make "ADD_REVISION=1" "OCAMLOPT=x86_64-apple-darwin11-ocamlopt.opt" "OCAMLC=x86_64-apple-darwin11-ocamlopt.opt" && cp haxe $MAC/build/haxe.exe && cp -rf std $MAC/build/ && echo "#!/bin/sh" > $MAC/build/haxelib && echo "haxe --run tools.haxelib.Main %*" >> $MAC/build/haxelib && exit 0
 exit 1
