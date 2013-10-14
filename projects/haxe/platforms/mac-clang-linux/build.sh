0578409b420fc1e39b33ea32709b0af773f961ab
Caue Waneck <waneck@gmail.com>
2013-10-14 00:11:02 -0300

    added mac-linux

---
+++ projects/haxe/platforms/mac-clang-linux/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+MAC=$PWD
+
+if [ $# -eq 0 ]; then
+        echo "No arguments supplied: $1"
+        exit 1
+fi
+
+cd ../../repo/haxe
+rm -f haxe*
+make clean && make "ADD_REVISION=1" "OCAMLOPT=x86_64-apple-darwin11-ocamlopt.opt" "OCAMLC=x86_64-apple-darwin11-ocamlopt.opt" && cp haxe $MAC/build/haxe.exe && cp -rf std $MAC/build/ && echo "#!/bin/sh" > $MAC/build/haxelib && echo "haxe --run tools.haxelib.Main %*" >> $MAC/build/haxelib && exit 0
+exit 1
