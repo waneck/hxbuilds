433640e5c49308fc8ff36f355fb01f91261cceed
Caue Waneck <waneck@gmail.com>
2013-11-05 10:41:28 -0200

    added win64 builds

---
+++ projects/haxe/platforms/windows64-mingw-linux/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+WIN=$PWD
+
+if [ $# -eq 0 ]; then
+        echo "No arguments supplied: $1"
+        exit 1
+fi
+
+cd ../../repo/haxe
+rm -f haxe*
+make clean && make "ADD_REVISION=1" "OCAMLOPT=x86_64-w64-mingw32-ocamlopt" "OCAMLC=x86_64-w64-mingw32-ocamlc" && cp haxe $WIN/build/haxe.exe && cp -rf std $WIN/build/ && echo "haxe --run tools.haxelib.Main %*" > $WIN/build/haxelib.bat && exit 0
+exit 1
