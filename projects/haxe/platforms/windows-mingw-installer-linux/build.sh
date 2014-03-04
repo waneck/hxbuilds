b95fedb4e6cb9d1d546a7fd6425bde32ea23e922
Caue Waneck <waneck@gmail.com>
2014-03-03 23:08:41 -0500

    consolidate haxelib

--- projects/haxe/platforms/windows-mingw-installer-linux/build.sh
+++ projects/haxe/platforms/windows-mingw-installer-linux/build.sh
@@ -18,18 +18,28 @@ if [ $BRANCH == "master" ]; then
 fi
 
 rm -f haxe*
-make clean && make "ADD_REVISION=$ADDREV" "OCAMLOPT=i686-w64-mingw32-ocamlopt" "OCAMLC=i686-w64-mingw32-ocamlc" && cp haxe $WIN/tmp/resources/haxe/haxe.exe && cp -rf std $WIN/tmp/resources/haxe/ && echo "haxe --run tools.haxelib.Main %*" > $WIN/tmp/resources/haxe/haxelib.bat
+make clean && make "ADD_REVISION=$ADDREV" "OCAMLOPT=i686-w64-mingw32-ocamlopt" "OCAMLC=i686-w64-mingw32-ocamlc" && cp haxe $WIN/tmp/resources/haxe/haxe.exe && cp -rf std $WIN/tmp/resources/haxe/
 if [ $? -neq 0 ]; then
   exit 1
 fi
 
+# extra
 cp extra/*.txt $WIN/tmp/resources/haxe
 cp -rf extra/haxelib_src/src/
 cp extra/*.nsi $WIN/tmp
 cp extra/*.nsh $WIN/tmp
 cp -rf extra/images $WIN/tmp
+
+# neko
 tar -zxvf $WIN/neko-stable.tar.gz -C $WIN/tmp/resources
 mv $WIN/tmp/resources/neko* $WIN/tmp/resources/neko
+
+# haxelib
+cd extra/haxelib_src
+haxe haxelib.hxml
+cp bin/haxelib.n $WIN/tmp/resources/haxe
+cd $WIN/tmp/resources/haxe
+neko $WIN/../../platforms/common/boot.n -b ../neko/neko.exe haxelib.n
 
 # docs
 cd $WIN/repo/dox
--- projects/haxe/platforms/windows-mingw-installer-linux/setup.sh
+++ projects/haxe/platforms/windows-mingw-installer-linux/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 sudo apt-get install nsis
+mkdir repo
+git clone https://github.com/dpeek/dox.git repo/dox
