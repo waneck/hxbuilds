0e916b9f30f249935a452f252c727db9876e93b9
Your Name <you@example.com>
2014-03-04 02:10:03 -0500

    parameterized installer structure

--- projects/haxe/platforms/mac-clang-installer-linux/build.sh
+++ projects/haxe/platforms/mac-clang-installer-linux/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WIN=$PWD
+MAC=$PWD
 if [ $# -lt 2 ]; then
 	echo "Too few arguments supplied: $1"
 	exit 1
@@ -9,9 +9,11 @@ fi
 REV=$1
 VER=$2
 VERLONG=$2
+NEKOVER=2.0.0
 
 rm -rf tmp
-mkdir -p tmp/resources/haxe/doc
+mkdir -p tmp/haxe/doc
+mkdir -p tmp/installer
 
 if [ ! -e neko-stable.tar.gz ]; then
   echo "In order to have a working installer, you need to provide a neko directory with the desired version"
@@ -33,30 +35,26 @@ CLEANVER=${2//[^0-9\.]/}
 echo "Installer for $VER ($CLEANVER)"
 
 rm -f haxe*
-(make clean && make "ADD_REVISION=$ADDREV" "OCAMLOPT=i686-w64-mingw32-ocamlopt" "OCAMLC=i686-w64-mingw32-ocamlc" && cp haxe $WIN/tmp/resources/haxe/haxe.exe && cp -rf std $WIN/tmp/resources/haxe/) || exit 1
-i686-w64-mingw32-g++ -static extra/setup.cpp -o $WIN/tmp/resources/haxe/haxesetup.exe || exit 1
+export MACOSX_DEPLOYMENT_TARGET=10.5
+(make clean && make "ADD_REVISION=$ADDREV" "OCAMLOPT=x86_64-apple-darwin11-ocamlopt.opt" "OCAMLC=x86_64-apple-darwin11-ocamlopt.opt" && cp haxe $MAC/tmp/haxe && cp -rf std $MAC/tmp/haxe/) || exit 1
 
 # extra
-cp extra/*.txt $WIN/tmp/resources/haxe
-cp extra/*.nsi $WIN/tmp
-cp extra/*.nsh $WIN/tmp
-cp -rf extra/images $WIN/tmp
+cp extra/*.txt $MAC/tmp/haxe
 
 # neko
-tar -zxvf $WIN/neko-stable.tar.gz -C $WIN/tmp/resources
-mv $WIN/tmp/resources/neko* $WIN/tmp/resources/neko
+tar -zxvf $MAC/neko-stable.tar.gz -C $MAC/tmp
+mv $MAC/tmp/neko* $MAC/tmp/neko
 
 # haxelib
 cd extra/haxelib_src
 haxe haxelib.hxml
-cp bin/haxelib.n $WIN/tmp/resources/haxe
-cd $WIN/tmp/resources/haxe
-neko $WIN/../../platforms/common/boot.n -b ../neko/neko.exe haxelib.n
-mv haxelib haxelib.exe
+cp bin/haxelib.n $MAC/tmp/haxe
+cd $MAC/tmp/haxe
+neko $MAC/../../platforms/common/boot.n -b ../neko/neko haxelib.n
 rm haxelib.n
 
 # docs
-cd $WIN/repo/dox
+cd $MAC/repo/dox
 git checkout -- .
 git fetch
 git pull
@@ -64,14 +62,15 @@ haxelib dev dox .
 rm -rf bin/pages/*
 haxe gen.hxml || exit 1
 haxe std.hxml || exit 1
-cp -Rf bin/pages/* $WIN/tmp/resources/haxe/doc || exit 1
+cp -Rf bin/pages/* $MAC/tmp/haxe/doc || exit 1
 
 # ready to execute!
-cd $WIN/tmp
-sed -i "s/%%VERSION%%/$CLEANVER/g" installer.nsi
-sed -i "s/%%VERSTRING%%/$VER/g" installer.nsi
-sed -i "s/%%VERLONG%%/$VERLONG/g" installer.nsi
-makensis installer.nsi || exit 1
-cp *.exe ../build
+cd $MAC/tmp/installer
+xar -xf ../../installer-structure.pkg .
+sed -i "s/%%VERSION%%/$CLEANVER/g" Distribution
+sed -i "s/%%VERSTRING%%/$VER/g" Distribution
+sed -i "s/%%VERLONG%%/$VERLONG/g" Distribution
+sed -i "s/%%NEKOVER%%/$NEKOVER/g" Distribution
+
 
 exit 0
