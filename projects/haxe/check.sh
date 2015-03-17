1beb01f870eb52af8e6b21740f92c16967698337
Cauê Waneck <waneck@gmail.com>
2015-03-17 07:11:28 -0300

    only checkout from origin AFTER first rev

--- projects/haxe/check.sh
+++ projects/haxe/check.sh
@@ -4,12 +4,12 @@ if [ ! -e ../.updated ]; then
 	BRANCH=$(git rev-parse --abbrev-ref HEAD)
 	if [ $BRANCH == "development" ]; then
 		echo "checking master"
-		git checkout origin/master
-		git checkout -B master
+		git checkout master
+		TARGET_BRANCH="master"
 	else
 		echo "checking development"
-		git checkout origin/development
-		git checkout -B development
+		git checkout development
+		TARGET_BRANCH="development"
 	fi
 fi
 
@@ -19,6 +19,8 @@ git submodule update --force --recursive
 git fetch
 git pull origin `git name-rev --name-only HEAD` || exit 1
 git submodule update --force --recursive || exit 1
+git checkout origin/$TARGET_BRANCH
+git checkout -B $TARGET_BRANCH
 VER=$(git rev-list --tags --max-count=1)
 REV=$(git rev-parse HEAD)
 if [ ! $OLDREV = $REV ]; then
